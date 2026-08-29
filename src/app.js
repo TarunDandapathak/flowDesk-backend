5f4f5732a65b58b526fb681332b6f0eda8fbf3f9
TarunDandapathak <tarundandapathak25@gmail.com>
2026-08-29 12:24:27 +0530

    Update CORS origin to production URL

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://flowdesk-ten-henna.vercel.app",
     credentials: true
 }));
 app.use(express.json());
